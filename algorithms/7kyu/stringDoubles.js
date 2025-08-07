e645c11436dbd23f0e3b1d4283bf4fc4a707b105
elyse502 <elyseniyibizi502@gmail.com>
2025-08-07 12:48:29 +0300

    Add doubles - a function remove double string characters that are adjacent to each other

---
+++ algorithms/7kyu/stringDoubles.js
@@ -0,0 +1,13 @@
+exports.doubles = (s) => {
+    const stack = [];
+
+    for (const char of s) {
+        if (stack.length && stack[stack.length - 1] === char) {
+            stack.pop(); // remove the previous matching character
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join('');
+}
---
+++ algorithms/7kyu/tests/stringDoubles.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const doubles = require('../stringDoubles').doubles;
+
+console.log(doubles('abbbzz'));
+console.log(doubles('zzzzykkkd'));
+console.log(doubles('abbcccdddda'));
+console.log(doubles('vvvvvoiiiiin'));
+console.log(doubles('rrrmooomqqqqj'));
+console.log(doubles('xxbnnnnnyaaaaam'));
+
