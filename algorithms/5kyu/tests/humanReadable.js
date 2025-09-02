ddc8976319dcd3d8d2022378ee0748894ed89534
elyse502 <elyseniyibizi502@gmail.com>
2025-09-02 20:23:26 +0300

    feat: Add a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)

---
+++ algorithms/5kyu/tests/humanReadable.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const humanReadable = require("../humanReadable").humanReadable;
+
+console.log(humanReadable(0));
+console.log(humanReadable(59));
+console.log(humanReadable(60));
+console.log(humanReadable(90));
+console.log(humanReadable(3599));
+console.log(humanReadable(3600));
+console.log(humanReadable(45296));
+console.log(humanReadable(86399));
+console.log(humanReadable(86400));
+console.log(humanReadable(359999));
