5fa50f48acdc433194398bd1d463230075a10934
elyse502 <elyseniyibizi502@gmail.com>
2025-09-03 18:36:56 +0300

    feat: Add rgb function

---
+++ algorithms/5kyu/tests/rgb.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const rgb = require("../rgb").rgb;
+
+console.log(rgb(255, 255, 255));
+console.log(rgb(255, 255, 300));
+console.log(rgb(0, 0, 0));
+console.log(rgb(10, 0, 256));
+console.log(rgb(148, 0, 211));
+console.log(rgb(0, 0, -20));
+console.log(rgb(173, 255, 47));
