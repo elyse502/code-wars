0c3bb320b38143b06d129a1674415ce105e9aea8
elyse502 <elyseniyibizi502@gmail.com>
2025-09-10 12:41:53 +0300

    feat: Add a function that takes a list of integers in increasing order and returns a correctly formatted string in the range format

---
+++ algorithms/4kyu/tests/rangeExtraction.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const rangeExtraction = require("../rangeExtraction").solution;
+
+console.log(
+  rangeExtraction([
+    -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20,
+  ])
+);
