c5daac3bce0a651a8f91d0678fb7dec48c2f0dcf
elyse502 <elyseniyibizi502@gmail.com>
2025-10-22 12:17:11 +0300

    feat: implement function to sum multiples of 3 or 5 below a given number

---
+++ algorithms/6Kyu/tests/multiplesOf3Or5.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { solution } = require("../multiplesOf3Or5");
+
+console.log(solution(10));
+console.log(solution(23));
+console.log(solution(45));
+console.log(solution(90));
+console.log(solution(122));
