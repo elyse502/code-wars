79cff76d40be80bfd2eecf754e3874a2941f0080
elyse502 <elyseniyibizi502@gmail.com>
2025-10-23 12:24:33 +0200

    feat: implement spinWords function for reversing long words

---
+++ algorithms/6Kyu/tests/spinWords.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const { spinWords } = require("../spinWords");
+
+console.log(spinWords("Hey fellow warriors"));
+console.log(spinWords("This is a test     "));
+console.log(spinWords("This is another test"));
