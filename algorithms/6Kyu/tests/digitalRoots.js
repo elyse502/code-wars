1c338dcfdfebd74dd22834d883b91175179cc238
elyse502 <elyseniyibizi502@gmail.com>
2025-10-23 12:32:48 +0200

    feat: implement digitalRoot function to compute digital sum recursively

---
+++ algorithms/6Kyu/tests/digitalRoots.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const { digitalRoot } = require("../digitalRoots");
+
+console.log(digitalRoot(16));
+console.log(digitalRoot(942));
+console.log(digitalRoot(132189));
+console.log(digitalRoot(493193));
