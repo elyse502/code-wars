a4a4726dccb5435e92503a00a28dcf897091d795
elyse502 <elyseniyibizi502@gmail.com>
2025-10-24 13:10:21 +0300

    feat: implement efficient findOutlier function to detect numeric parity outlier in an array

---
+++ algorithms/6Kyu/tests/findTheParityOutlier.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { findOutlier } = require("../findTheParityOutlier");
+
+console.log(findOutlier([0, 1, 2]));
+console.log(findOutlier([1, 2, 3]));
+console.log(findOutlier([2, 6, 8, 10, 3]));
+console.log(findOutlier([0, 0, 3, 0, 0]));
+console.log(findOutlier([1, 1, 0, 1, 1]));
