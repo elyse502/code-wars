354ea5556fb84eb14fd90a67c5cee154d94cb5d8
elyse502 <elyseniyibizi502@gmail.com>
2025-10-29 11:32:47 +0200

    feat(strings): implement string combination with length-based ordering

---
+++ algorithms/8kyu/tests/shortLongShort.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const { solution } = require("../shortLongShort");
+
+console.log(solution("1", "22"));
+console.log(solution("22", "1"));
+console.log(solution("aaa", "b"));
+console.log("--");
+console.log(solution("45", "1"));
+console.log(solution("13", "200"));
+console.log(solution("Soon", "Me"));
+console.log(solution("U", "False"));
