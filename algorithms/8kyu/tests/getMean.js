892b7ca833092976bff646ccf39b0af132525d34
elyse502 <elyseniyibizi502@gmail.com>
2025-10-29 11:21:02 +0200

    feat(math): implement average calculator with floor rounding

---
+++ algorithms/8kyu/tests/getMean.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const { getAverage } = require("../getMean");
+
+console.log(getAverage([2, 2, 2, 2]));
+console.log(getAverage([1, 2, 3, 4, 5]));
+console.log(getAverage([1, 1, 1, 1, 1, 1, 1, 2]));
