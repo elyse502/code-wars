20148adf062ad8a30b221e3f8a6932ea4c1c5ad9
elyse502 <elyseniyibizi502@gmail.com>
2025-10-30 12:55:49 +0300

    feat(strings): implement first non-repeating character finder

---
+++ algorithms/5kyu/tests/firstNonRepeatingLetter.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { firstNonRepeatingLetter } = require("../firstNonRepeatingLetter");
+
+console.log(firstNonRepeatingLetter("sTreSS"));
+console.log(firstNonRepeatingLetter("stress"));
+console.log(firstNonRepeatingLetter(""));
+console.log(firstNonRepeatingLetter("moonmen"));
+console.log(firstNonRepeatingLetter("a"));
