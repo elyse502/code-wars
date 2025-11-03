c7712f9b49bc74b1a19708961cdc3da2a0e8df68
elyse502 <elyseniyibizi502@gmail.com>
2025-11-03 12:07:27 +0200

    feat(math): implement efficient prime number checker function

---
+++ algorithms/6Kyu/tests/isPrime.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { isPrime } = require("../isPrime");
+
+console.log(isPrime(0));
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(73));
+console.log(isPrime(75));
+console.log(isPrime(-1));
