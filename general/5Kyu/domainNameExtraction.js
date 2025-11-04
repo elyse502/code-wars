519b7021f2a8f7cf69fc2d73b2fe3b5a14dde36d
elyse502 <elyseniyibizi502@gmail.com>
2025-11-04 11:23:08 +0200

    feat(utils): add domainName function to extract base domain from URL

---
+++ general/5Kyu/domainNameExtraction.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * Extract the domain name from a URL
+ * 
+ * Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+    - url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+    - url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+    - url = "https://www.cnet.com"                -> domain name = cnet"
+ * 
+ * @param {*} url 
+ * @returns {string}
+ */
+
+exports.domainName = (url) => {
+  url = url.replace("http://", "").replace("https://", "").replace("www.", "");
+
+  return url.split(".")[0];
+};
---
+++ general/5Kyu/tests/domainNameExtraction.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { domainName } = require("../domainNameExtraction");
+
+console.log(domainName("http://google.com"));
+console.log(domainName("http://google.co.jp"));
+console.log(domainName("www.xakep.ru"));
+console.log(domainName("https://youtube.com"));
