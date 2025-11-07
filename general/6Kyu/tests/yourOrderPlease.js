a9001088992cead49ec18d9bb4e888373f6f339a
elyse502 <elyseniyibizi502@gmail.com>
2025-11-07 15:25:12 +0200

    feat(strings): implement word sorting by embedded numbers

---
+++ general/6Kyu/tests/yourOrderPlease.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { order } = require("../yourOrderPlease");
+
+console.log(order("is2 Thi1s T4est 3a"));
+console.log(order("4of Fo1r pe6ople g3ood th5e the2"));
+console.log(order(""));
+console.log(order("word1 word2 word3"));
+console.log(order("a1 b2 c3 d4 e5 f6 g7 h8 i9"));
