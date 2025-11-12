771e4112125f3f46e449beea85e55c97d8de77d6
elyse502 <elyseniyibizi502@gmail.com>
2025-11-12 11:40:26 +0200

    feat: add digit squaring and concatenation function

---
+++ general/7Kyu/tests/squareEveryDigit.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { squareDigits } = require("../squareEveryDigit");
+
+console.log(squareDigits(3212));
+console.log(squareDigits(2112));
+console.log(squareDigits(9119));
+console.log(squareDigits(0));
