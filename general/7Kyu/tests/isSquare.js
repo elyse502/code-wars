2aebf64b5de947ea05a75f1637498b38f00bfa9b
elyse502 <elyseniyibizi502@gmail.com>
2025-11-12 12:03:05 +0200

    feat: add perfect square validation function

---
+++ general/7Kyu/tests/isSquare.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { isSquare } = require("../isSquare");
+
+console.log(isSquare(-1));
+console.log(isSquare(0));
+console.log(isSquare(3));
+console.log(isSquare(4));
+console.log(isSquare(25));
+console.log(isSquare(26));
