874214d1f82bf8be79f45f3758da0290ad8b5f84
elyse502 <elyseniyibizi502@gmail.com>
2025-11-12 12:04:46 +0200

    feat: add middle character extractor function

---
+++ general/7Kyu/tests/getMiddleCharacter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { getMiddle } = require("../getMiddleCharacter");
+
+console.log(getMiddle("test"));
+console.log(getMiddle("testing"));
+console.log(getMiddle("middle"));
+console.log(getMiddle("A"));
