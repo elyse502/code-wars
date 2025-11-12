67a64473dc9c154d6b805111b0555afed3b74edc
elyse502 <elyseniyibizi502@gmail.com>
2025-11-12 11:34:26 +0200

    feat: add troll comment neutralizer with vowel removal

---
+++ general/7Kyu/tests/disemvowelTrolls.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const { disemvowel } = require("../disemvowelTrolls");
+
+console.log(disemvowel("This website is for losers LOL!"));
+console.log(
+  disemvowel("No offense but,\nYour writing is among the worst I've ever read")
+);
+console.log(disemvowel("What are you, a communist?"));
