52c3f8421b310919670a94f8a48a88a9d5861ee7
elyse502 <elyseniyibizi502@gmail.com>
2025-11-17 12:33:19 +0200

    fix: correct sheep counting logic with proper accumulator handling

---
+++ general/8Kyu/tests/countSheeps.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+const { countSheeps } = require("../countSheeps");
+
+console.log(countSheeps([]));
+console.log(countSheeps([undefined]));
+console.log(countSheeps([null]));
+console.log(countSheeps([false]));
+console.log(countSheeps([true]));
+console.log(countSheeps([undefined, null, false, true]));
+console.log(
+  countSheeps([undefined, null, false, true, true, false, null, undefined])
+);
+console.log(
+  countSheeps([
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    true,
+    true,
+  ])
+);
