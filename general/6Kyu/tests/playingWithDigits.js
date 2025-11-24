521cf3532ac3285a0b69cb308e93520401bf1bf4
elyse502 <elyseniyibizi502@gmail.com>
2025-11-24 11:21:58 +0200

    feat: add digit power sum calculator with divisibility checker

---
+++ general/6Kyu/tests/playingWithDigits.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const { digPow } = require("../playingWithDigits");
+
+console.log(digPow(89, 1));
+console.log(digPow(92, 1));
+console.log(digPow(695, 2));
+console.log(digPow(46288, 3));
