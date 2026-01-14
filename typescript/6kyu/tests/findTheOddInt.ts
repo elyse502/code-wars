2c914200aeb5b114b51c03f2d08b588f26f003f8
elyse502 <elyseniyibizi502@gmail.com>
2026-01-14 11:18:30 +0200

    feat: implement findOdd using XOR for optimal odd-occurrence detection

---
+++ typescript/6kyu/tests/findTheOddInt.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env tsx
+import { findOdd } from "../findTheOddInt";
+
+console.log(findOdd([1, 1, 2]));
+console.log(findOdd([0, 1, 0, 1, 0]));
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
