1e54ea1d7ac06bc0c9f61e6c19c69c5082c07e05
elyse502 <elyseniyibizi502@gmail.com>
2026-01-19 11:58:06 +0200

    feat(tribonacci): implement efficient Tribonacci sequence generator

---
+++ typescript/6kyu/tests/tribonacciSequence.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env tsx
+import { tribonacci } from "../tribonacciSequence";
+
+console.log(tribonacci([1, 1, 1], 10));
+console.log(tribonacci([0, 0, 1], 10));
+console.log(tribonacci([3, 2, 1], 0));
