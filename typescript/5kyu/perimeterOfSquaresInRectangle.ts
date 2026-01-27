40e9c4479ca1d0ce1be8ca4e2747837a6734037d
elyse502 <elyseniyibizi502@gmail.com>
2026-01-27 11:21:32 +0200

    feat(math): add efficient Fibonacci-based perimeter calculation for squares

---
+++ typescript/5kyu/perimeterOfSquaresInRectangle.ts
@@ -0,0 +1,38 @@
+/**
+ * Perimeter of squares in a rectangle
+ * 
+ * The drawing shows 6 squares the sides of which have a length of 1, 1, 2, 3, 5, 8. It's easy to see that the sum of the perimeters of these squares is : 4 * (1 + 1 + 2 + 3 + 5 + 8) = 4 * 20 = 80 
+
+ * Could you give the sum of the perimeters of all the squares in a rectangle when there are n + 1 squares disposed in the same manner as in the drawing:
+
+    alternative text
+
+ * Hint:
+    * See Fibonacci sequence
+
+ * Ref:
+    http://oeis.org/A000045
+
+ * The function perimeter has for parameter n where n + 1 is the number of squares (they are numbered from 0 to n) and returns the total perimeter of all the squares.
+
+    perimeter(5)  should return 80
+    perimeter(7)  should return 216
+ * 
+ * @param n 
+ * @returns 
+ */
+
+export const perimeter = (n: number): number => {
+  let a = 1;
+  let b = 1;
+  let sum = a + b; // F(0) + F(1)
+
+  for (let i = 2; i <= n; i++) {
+    const next = a + b;
+    sum += next;
+    a = b;
+    b = next;
+  }
+
+  return 4 * sum;
+};
---
+++ typescript/5kyu/tests/perimeterOfSquaresInRectangle.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env tsx
+import { perimeter } from "../perimeterOfSquaresInRectangle";
+
+console.log(perimeter(5)); // 80
+console.log(perimeter(7)); // 216
