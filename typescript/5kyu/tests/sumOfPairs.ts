6b3389fde0a3cef1b0d907cbfec752c032036c7d
elyse502 <elyseniyibizi502@gmail.com>
2026-01-30 11:44:58 +0200

    feat(sum-pairs): implement efficient first-pair lookup with linear time complexity

---
+++ typescript/5kyu/tests/sumOfPairs.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env tsx
+import { sumPairs } from "../sumOfPairs";
+
+console.log(sumPairs([11, 3, 7, 5], 10)); // [3, 7]
+console.log(sumPairs([4, 3, 2, 3, 4], 6)); // [4, 2]
+console.log(sumPairs([0, 0, -2, 3], 2)); // undefined
+console.log(sumPairs([10, 5, 2, 3, 7, 5], 10)); // [3, 7]
