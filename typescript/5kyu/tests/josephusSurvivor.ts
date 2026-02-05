4af8711d6a4d3741a4c37dbedb6f4e9af5fcf0c9
elyse502 <elyseniyibizi502@gmail.com>
2026-02-05 12:24:06 +0200

    feat(josephus): implement efficient survivor calculation using iterative formula

---
+++ typescript/5kyu/tests/josephusSurvivor.ts
@@ -0,0 +1,8 @@
+#!/usr/bin/env tsx
+import { josephusSurvivor } from "../josephusSurvivor";
+
+console.log(josephusSurvivor(7, 3)); // 4
+console.log(josephusSurvivor(1, 1)); // 1
+console.log(josephusSurvivor(5, 2)); // 3
+console.log(josephusSurvivor(10, 3)); // 4
+console.log(josephusSurvivor(40, 3)); // 28
