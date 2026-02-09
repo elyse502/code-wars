6d00e8a7e15b7c92b80460340e791ef666a268c2
elyse502 <elyseniyibizi502@gmail.com>
2026-02-09 12:18:03 +0200

    feat: implement findMissingLetter using character code comparison

---
+++ typescript/6kyu/tests/findTheMissingLetter.ts
@@ -0,0 +1,5 @@
+#!/usr/bin/env tsx
+import { findMissingLetter } from "../findTheMissingLetter";
+
+console.log(findMissingLetter(["a", "b", "c", "d", "f"])); // 'e'
+console.log(findMissingLetter(["O", "Q", "R", "S"])); // 'P'
