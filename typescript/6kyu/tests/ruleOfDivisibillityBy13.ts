0c888ec529b0a7707f14eae49303475413ea0a0e
elyse502 <elyseniyibizi502@gmail.com>
2026-02-16 11:50:07 +0200

    feat(thirt): implement stationary remainder reducer using mod-13 weight cycle

---
+++ typescript/6kyu/tests/ruleOfDivisibillityBy13.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env tsx
+import { thirt } from "../ruleOfDivisibillityBy13";
+
+console.log(thirt(1234567)); // 87
+console.log(thirt(321)); // 48
+console.log(thirt(8529)); // 79
+console.log(thirt(85299258)); // 31
+console.log(thirt(5634)); // 57
+console.log(thirt(0)); // 0
